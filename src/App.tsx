e8bbb46df7fdbfac9310d241ec0b58e14213e94c
skippi <jbtcao@gmail.com>
2024-04-08 22:07:30 -0500

    move stratagem data to data.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,60 @@
 import { MdOutlineContentCopy } from "react-icons/md";
-import './App.css';
-import Booster from './components/Booster';
-import Item from './components/Item';
-import Stratagem from './components/Stratagem';
+import "./App.css";
+import Booster from "./components/Booster";
+import Item from "./components/Item";
+import Stratagem from "./components/Stratagem";
+import { stratagemImages } from "./data";
+import { useState } from "react";
+
+function randomStratagemId() {
+  const items = Object.keys(stratagemImages);
+  return items[Math.floor(Math.random() * items.length)];
+}
 
 function App() {
+  const [stratagems, setStratagems] = useState([
+    randomStratagemId(),
+    randomStratagemId(),
+    randomStratagemId(),
+    randomStratagemId(),
+  ]);
+
+  const randomizeButtonClick = () => {
+    setStratagems(stratagems.map(() => randomStratagemId()))
+  }
+
   return (
     <div className="main">
-      <div className='title'>HELLPOD.IO</div>
-      <div className='subtitle'>Helldivers 2 Builder</div>
-      <div className='itemGrid'>
+      <div className="title">HELLPOD.IO</div>
+      <div className="subtitle">Helldivers 2 Builder</div>
+      <div className="itemGrid">
         <Item />
         <Item />
         <Item />
         <Item />
       </div>
-      <div className='stratagemGrid'>
-        <Stratagem option="arc-3-arc-thrower" />
-        <Stratagem option="ax/las-5-guard-dog-rover" />
-        <Stratagem option="eagle-airstrike" />
-        <Stratagem option="eagle-500kg-bomb" />
+      <div className="stratagemGrid">
+        {stratagems.map((id) => (
+          <Stratagem option={id} />
+        ))}
       </div>
       <div className="boosterSection">
         <Booster />
-        <div className='boosterName'>INCREASED REINFORCEMENT BUDGET</div>
+        <div className="boosterName">INCREASED REINFORCEMENT BUDGET</div>
       </div>
-      <div className='shareSection'>
-        <div className='randomizeButton'>RANDOMIZE</div>
-        <div className='shareButton'>
+      <div className="shareSection">
+        <div className="randomizeButton" onClick={randomizeButtonClick}>RANDOMIZE</div>
+        <div className="shareButton">
           <MdOutlineContentCopy color="#5D854A" size={32} />
         </div>
       </div>
-      <div className='footer'>
+      <div className="footer">
         © skippi, 2024
         <br />
         All Helldivers art belongs to Arrowhead Studios
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Stratagem.tsx
+++ src/components/Stratagem.tsx
@@ -1,22 +1,5 @@
 import "./Stratagem.css";
-
-const stratagemImages: {
-  [id: string]: string;
-} = {};
-
-const stratagemAssets = Object.values(
-  import.meta.glob("../assets/stratagems/*.{png,PNG}", {
-    eager: true,
-    as: "url",
-  }),
-);
-
-for (const path of stratagemAssets) {
-  const url = new URL(path, import.meta.url).href;
-  const name = decodeURI(path).split("/").reverse()[0].replace(/\.[^/.]+$/, "").replace("_", "/").replace("\"", "");
-  const id = name.replace(/\s+/g, "-").toLowerCase();
-  stratagemImages[id] = url
-}
+import { stratagemImages } from "@/data"
 
 function Stratagem({ option }: { option?: string }) {
   let img = null;
