f50c1d754efd4b7a376e3ff48ec5030bb3d367fe
skippi <jbtcao@gmail.com>
2024-04-09 00:53:14 -0500

    add item hint to Item component
    Still on the fence whether this should show the full name or just the
    code name.

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -10,6 +10,11 @@ function Item({ option }: { option?: string }) {
     <div className="item">
       <div className="itemInner">
         {entry && <img className="itemIcon" src={entry.image} />}
+        {entry && (
+          <div className="itemHint">
+            {entry.name.substr(0, entry.name.indexOf(" ") + 1)}
+          </div>
+        )}
       </div>
     </div>
   );
