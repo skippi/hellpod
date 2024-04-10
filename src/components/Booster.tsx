76231a33b6f7cf197f7e450da1b3b10eb2c9a5fd
skippi <jbtcao@gmail.com>
2024-04-09 19:11:11 -0500

    add hover highlighting to display components

--- src/components/Booster.tsx
+++ src/components/Booster.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./Booster.css";
 import { boosterData } from "@/data";
 
+const defaultBorderColor = "#676767";
+const hoverBorderColor = "#a7a7a7";
+
 function Booster({ option }: { option?: string }) {
+  const [borderColor, setBorderColor] = useState(defaultBorderColor);
   let entry = null;
   if (option && option in boosterData) {
     entry = boosterData[option];
   }
   return (
-    <div className="booster">
+    <div
+      className="booster"
+      onMouseEnter={() => setBorderColor(hoverBorderColor)}
+      onMouseLeave={() => setBorderColor(defaultBorderColor)}
+    >
       <svg
         className="boosterInner"
         width="97"
@@ -23,7 +32,7 @@ function Booster({ option }: { option?: string }) {
           fillRule="evenodd"
           clipRule="evenodd"
           d="M5 43L27.5 4.02887H72.5L95 43L72.5 81.9712H27.5L5 43ZM0.381195 43L25.1906 0.0288696H74.8094L99.6188 43L74.8094 85.9712H25.1906L0.381195 43Z"
-          fill="#676767"
+          fill={borderColor}
         />
       </svg>
       {entry && <img className="boosterIcon" src={entry.image} />}
